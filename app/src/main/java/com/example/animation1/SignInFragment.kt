b0c3ada0f42c0b56fc38b46c16ff53810790d680
Khalil Mensi <mensikhalil@gmail.com>
2020-12-22 14:46:38 +0100

    test des champs en cas erreur de saisie par l'utilisateur

--- app/src/main/java/com/example/animation1/SignInFragment.kt
+++ app/src/main/java/com/example/animation1/SignInFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_signin.*
 import kotlinx.android.synthetic.main.fragment_signin.view.*
@@ -22,7 +25,15 @@ class SignInFragment : Fragment() {
         signup.setOnClickListener(View.OnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_signInFragment_to_signUpFragment);
         })
-
+        val signIn = view.findViewById<Button>(R.id.signUpBtn);
+        val pass = view.findViewById<EditText>(R.id.password)
+        signIn.setOnClickListener(View.OnClickListener{
+            if(testerChamps()) {
+                // TODO
+            } else {
+                Toast.makeText(context,"SignIn Failed !",Toast.LENGTH_SHORT).show()
+            }
+        })
         val forgotPass = view.findViewById<TextView>(R.id.forgotPass);
         forgotPass.setOnClickListener(View.OnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_signInFragment_to_forgotPassword);
@@ -30,4 +41,16 @@ class SignInFragment : Fragment() {
         return view;
     }
 
+    private fun testerChamps(): Boolean {
+        if (login.text.isEmpty()) {
+            login.setError("Invalid Email");
+            login.isFocused
+            return false ;
+        } else if (password.text.isEmpty()) {
+            password.setError("Invalid Password");
+            password.isFocused
+            return false ;
+        }
+        return true ;
+    }
 }
--- app/src/main/java/com/example/animation1/SignUpFragment.kt
+++ app/src/main/java/com/example/animation1/SignUpFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 class SignUpFragment : Fragment() {
 
@@ -17,7 +19,21 @@ class SignUpFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
         val signIn = view.findViewById<TextView>(R.id.signIn)
-
+        val signUp = view.findViewById<Button>(R.id.signUpBtn);
+        signUp.setOnClickListener(View.OnClickListener {
+            if(userName.text.isEmpty()){
+                userName.error = "Invalid userName !";
+                userName.isFocused
+            } else if(userEmail.text.isEmpty()){
+                userEmail.error = "Invalid Email";
+                userEmail.isFocused
+            } else if(userPassword.text.isEmpty() || !userPassword.text.isEmpty() && userRePassword.text != userPassword.text){
+                userPassword.error = "Invalid Password"
+                userPassword.isFocused
+                userRePassword.error = "Invalid Password"
+                userRePassword.isFocused
+            }
+        })
         signIn.setOnClickListener (  View.OnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_signUpFragment_to_signInFragment);
         }  )
